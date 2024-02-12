1e7e999c3d36f4135bee3a9b2876db2005042b75
Alan <alannh.21@gmail.com>
2024-02-11 18:33:21 -0800

    + rutas y homepage, + cambios de estilos para wordle

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to={"/wordle"}>Juega Wordle</Link>
+                </li>
+            </ul>
+        </>
+    );
+};
+export default Home;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { initializeDocumentHead } from '../src/utils/utilities';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { initializeDocumentHead } from "../src/utils/utilities";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 initializeDocumentHead();
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
