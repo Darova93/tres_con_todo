7c0891b0194180722c8d9e5e7b4428a504861bb9
Alan Navarro <alannh.21@gmail.com>
2024-03-01 16:51:07 -0800

    borrando archivos rezagados

--- .frontend1/src/games/wordle/wordleLettter.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const WordleLetter = ({ letter, index, className }: { letter: string; index: number; className: string }) => {
-    return <div className={`letter ${className}`}>{letter}</div>;
-};
-
-export default WordleLetter;
--- .frontend1/src/utils/constants.tsx
+++
@@ -1,3 +0,0 @@
-export const PATH_WORDLE_API_KUBO = "http://192.168.100.7:5000";
-export const PATH_WORDLE_API_DANI = "http://192.168.100.12:5000";
-export const IP_ALAN = "http://192.168.100.69";
