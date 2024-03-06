040f367ed2a8aa7245d136d3392c6586e1d44275
David Rodriguez Vazquez <darova93@gmail.com>
2024-03-05 20:24:44 -0800

    db import template

--- backend/public/load_rae.py
+++ backend/public/load_rae.py
@@ -1,20 +1,9 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from os import environ
-from database.connection import db_session
+from database.connection import init_db
 from database.models import WordleWord
 
-
-# try:
-#     client.admin.command('ping')
-#     print("Successfully connected to MongoDB!")
-#     word_collection = client.games.words
-
-#     id = 1
-#     with open("palabras_rae_completo.txt", "r", encoding="utf-8", newline="\n") as words_file:
-#         for word in words_file:
-#             u = WordleWord(word, '')
-#             db_session.add(u)
-#         db_session.commit()
-#     print(f"Exported {id} entries")
-# except Exception as e:
-#     print(e)
-
+print(WordleWord())
