22a36bdb7d938691da82391abde72934f6646b19
David Rodriguez Vazquez <darova93@gmail.com>
2024-03-05 20:33:04 -0800

    fix script

--- backend/public/load_rae.py
+++ backend/public/load_rae.py
@@ -3,14 +3,16 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from os import environ
-from database.connection import db_session
+from database.connection import db_session, init_db
 from database.models import Word
 
 try:
+    init_db()
     id = 1
-    with open("./palabras_rae.txt", "r", encoding="utf-8", newline="\n") as words_file:
+    with open("backend/public/palabras_rae.txt", "r", encoding="utf-8", newline="\n") as words_file:
         for name in words_file:
-            u = Word(word=name,locale="es_MX")
+            print(name)
+            u = Word(word=name, locale="es_MX")
             db_session.add(u)
             id += 1
         db_session.commit()
