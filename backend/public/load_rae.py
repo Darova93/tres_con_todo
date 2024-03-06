a5e9173566a65040b251c96a14d0b75815ed29b8
David Rodriguez Vazquez <darova93@gmail.com>
2024-03-05 20:26:41 -0800

    Add connection tempalte

--- backend/public/load_rae.py
+++ backend/public/load_rae.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('/home/danielalbinoms/Documents/tres_con_todo/backend')
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
 from os import environ
 from database.connection import db_session
